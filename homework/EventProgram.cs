def65bb28aa4d05190740c45a3217e6779fb716a
sagar0840 <sagarkale55556@gmail.com>
2023-04-13 17:37:31 +0530

    Add project files.

---
+++ homework/EventProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace homework
+{
+    internal class EventProgram
+    {
+        public EventProgram()
+        {
+        }
+
+        internal string MyEvent(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
